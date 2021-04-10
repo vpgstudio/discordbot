2e05ba0b0c19a5f1152b3156254ab9d6878b3fd5
Mackan <52716811+vpg-code@users.noreply.github.com>
2021-04-10 17:41:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const init = async () => {
   }
 
   // Here we login the client.
-  client.login(client.config.token);
+  client.login(process.env.TOKAN);
 
   // End top-level async/await function.
 };
